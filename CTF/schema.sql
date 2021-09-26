54600e535163d3007ef3116288d7019a8b9a7b07
Mark0519 <2578653827@qq.com>
2021-09-26 20:15:47 +0800

    sql文件

---
+++ CTF/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS user;
+-- DROP TABLE IF EXISTS flag;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+-- CREATE TABLE flag (
+--
+-- );
