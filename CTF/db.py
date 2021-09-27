ce778d9f2f6a00371852e7650763f41cf9eb2b35
Mark0519 <2578653827@qq.com>
2021-09-27 15:13:39 +0800

    pymysql

--- CTF/db.py
+++ CTF/db.py
@@ -1,14 +1,23 @@
-# import mysql.connector
+import pymysql
 
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-# db = mysql.connector.connect(
-#   host="localhost",       # 数据库主机地址
-#   user="root",    # 数据库用户名
-#   passwd="admin"   # 数据库密码
-# )
+# db=pymysql.connect(host="localhost",user="root",password="admin",database="test")
+# # 使用 cursor() 方法创建一个游标对象 cursor
+# cursor = db.cursor()
+#
+# # 使用 execute()  方法执行 SQL 查询
+# cursor.execute("SELECT VERSION()")
+#
+# # 使用 fetchone() 方法获取单条数据.
+# data = cursor.fetchone()
+#
+# print("Database version : %s " % data)
+#
+# # 关闭数据库连接
+# db.close()
 
 
 # def get_db():
