e8d9e75b737a7635fb4054085eff2bf9ceaaf5ce
Mark0519 <2578653827@qq.com>
2021-09-29 20:42:37 +0800

    CTFx主文件初始化程序

--- CTF/__init__.py
+++ CTF/__init__.py
@@ -31,10 +31,8 @@ def create_app(test_config=None):
 
     # from . import db
     # db.init_app(app)
-
     # from . import auth
     # app.register_blueprint(auth.bp)
-    #
     # return app
 
     from . import home
