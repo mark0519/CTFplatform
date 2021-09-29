feff74e5039acce98ba42c9e48f717755d2a74f7
Mark0519 <2578653827@qq.com>
2021-09-29 20:32:36 +0800

    完成home.html和flask的连接(但为完成是否登陆的判断和状态)

--- CTF/auth.py
+++ CTF/auth.py
@@ -3,9 +3,9 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+# from werkzeug.security import check_password_hash, generate_password_hash
 
-from CTF.db import get_db
+# from CTF.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
---
+++ CTF/home.py
@@ -0,0 +1,17 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+bp = Blueprint('home', __name__)
+
+
+@bp.route('/')
+def index():
+    # db = get_db()
+    # posts = db.execute(
+    #     'SELECT p.id, title, body, created, author_id, username'
+    #     ' FROM post p JOIN user u ON p.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+    return render_template('home/home.html')
