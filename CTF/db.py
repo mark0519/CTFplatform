f2fe82491cd83dca7caacdaa88f04181711de04d
Mark0519 <2578653827@qq.com>
2021-09-29 16:54:21 +0800

    数据库连接测试

--- CTF/db.py
+++ CTF/db.py
@@ -4,20 +4,25 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-# db=pymysql.connect(host="localhost",user="root",password="admin",database="test")
-# # 使用 cursor() 方法创建一个游标对象 cursor
-# cursor = db.cursor()
-#
-# # 使用 execute()  方法执行 SQL 查询
-# cursor.execute("SELECT VERSION()")
-#
-# # 使用 fetchone() 方法获取单条数据.
-# data = cursor.fetchone()
-#
-# print("Database version : %s " % data)
-#
-# # 关闭数据库连接
-# db.close()
+db = pymysql.connect(host="localhost", user="mark", password="stone", database="ctfdatebase")
+
+'''
+# 数据库连接测试
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# 使用 execute()  方法执行 SQL 查询
+cursor.execute("select * from test")
+
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+
+print("data == %s" % data)
+cursor.close()
+
+# 关闭数据库连接
+db.close()
+'''
 
 
 # def get_db():
