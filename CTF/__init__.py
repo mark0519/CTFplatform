bfad6c6b35d571f5198f5474b0c1501b5564bc52
Mark0519 <2578653827@qq.com>
2021-09-29 20:32:36 +0800

    前端web模板,和CTF文件下的主要文件无联系

--- CTF/__init__.py
+++ CTF/__init__.py
@@ -6,10 +6,10 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'CTF.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'CTF.sqlite'),
+    # )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -32,7 +32,13 @@ def create_app(test_config=None):
     # from . import db
     # db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+    # from . import auth
+    # app.register_blueprint(auth.bp)
+    #
+    # return app
+
+    from . import home
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- CTF/db.py
+++ CTF/db.py
@@ -6,23 +6,23 @@ from flask.cli import with_appcontext
 
 db = pymysql.connect(host="localhost", user="mark", password="stone", database="ctfdatebase")
 
-'''
-# 数据库连接测试
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
 
-# 使用 execute()  方法执行 SQL 查询
-cursor.execute("select * from test")
+# # 数据库连接测试
+# # 使用 cursor() 方法创建一个游标对象 cursor
+# cursor = db.cursor()
+#
+# # 使用 execute()  方法执行 SQL 查询
+# cursor.execute("select * from test")
+#
+# # 使用 fetchone() 方法获取单条数据.
+# data = cursor.fetchone()
+#
+# print("data == %s" % data)
+# cursor.close()
+#
+# # 关闭数据库连接
+# db.close()
 
-# 使用 fetchone() 方法获取单条数据.
-data = cursor.fetchone()
-
-print("data == %s" % data)
-cursor.close()
-
-# 关闭数据库连接
-db.close()
-'''
 
 
 # def get_db():
