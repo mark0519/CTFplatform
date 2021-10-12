f612563f53469dad8398fd2cb8f9ff883c976ff5
Mark0519 <2578653827@qq.com>
2021-10-12 20:00:37 +0800

    login前端完善和以及部分login后端

--- CTF/login.py
+++ CTF/login.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-=
+
+import hashlib
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -6,7 +10,7 @@ from werkzeug.exceptions import abort
 bp = Blueprint('/auth/login', __name__)
 
 
-@bp.route('/auth/login')
+@bp.route('/auth/login', methods=['GET', 'POST'])
 def index():
     # db = get_db()
     # posts = db.execute(
@@ -14,5 +18,39 @@ def index():
     #     ' FROM post p JOIN user u ON p.author_id = u.id'
     #     ' ORDER BY created DESC'
     # ).fetchall()
+
+    # 如果接收到数据
+    if request.method == 'POST':
+        account = request.form['name']
+        password = request.form['passwd']
+        md5 = hashlib.md5()
+        md5.update(password.encode('utf-8'))
+        password = md5.hexdigest()
+
+        if '@' in account:
+            email = account
+            mode = 0
+            print(email)
+        else:
+            username = account
+            mode = 1
+            print(username)
+        print("mode == "+str(mode))
+        print("password_md5 == " + password)
+        '''
+        mode = 0 是邮箱(email)登陆 
+        mode = 1 是用户名(username)登陆
+        email 是用户的邮箱
+        username 是用户名
+        使用其中一种方式登陆时另一个参数为空
+        password 是密码的md5值
+        '''
+
+        """
+        待完善.....
+        """
+
+
+
     return render_template('auth/login/login.html')
 
