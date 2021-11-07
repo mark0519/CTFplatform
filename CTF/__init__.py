30424c9cd91ba8badfea48072ed1f3dcde3bcda6
stone <1063764683@qq.com>
2021-11-07 19:53:41 +0800

    更改数据库用户名及密码

--- CTF/__init__.py
+++ CTF/__init__.py
@@ -20,7 +20,7 @@ def create_app(test_config=None):
 
     app.config['SECRET_KEY'] = 'ctfx'
     # 设置数据库连接url
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mark:stone@localhost:3306/ctf_database'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://adm1n:mark&stone@localhost:3306/ctf_database'
     # 设置这一项是每次请求结束后都会自动提交数据库中的变动
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
